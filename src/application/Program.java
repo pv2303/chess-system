7e128e6116f5339105ce8b8200a9a9e3b4449379
Paulo Vitor <paulovitoorr@outlook.com>
2026-04-17 19:27:41 -0300

    Cuidando das exceções

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,12 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+
+import java.util.InputMismatchException;
 
 public class Program {
     void main() {
@@ -13,19 +16,25 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.clearScreen(); // limpa a tela a cada rodada
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(IO.readln());
+            try {
+                UI.clearScreen(); // limpa a tela a cada rodada
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(IO.readln());
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(IO.readln());
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(IO.readln());
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            if (capturedPiece != null) {
-                System.out.println("Piece Captured: " + capturedPiece);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                if (capturedPiece != null) {
+                    System.out.println("Piece Captured: " + capturedPiece);
+                }
+            }
+            catch (ChessException | InputMismatchException e) {
+                IO.println(e.getMessage());
+                IO.readln(); // apertar a tecla enter
             }
         }
 
